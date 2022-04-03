178940642ffe7019d58d14089824d9faa1bb8ad2
eshita95 <eshita.islam95@gmail.com>
2022-04-04 01:52:02 +0600

    component and route added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './components/About/About';
+import Blogs from './components/Blogs/Blogs';
+import DashBoard from './components/DashBoard/DashBoard';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home'
+import NotFound from './components/NotFound/NotFound';
+import Reviwes from './components/Reviwes/Reviwes';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}>Home</Route>
+        <Route path='/reviwes' element={<Reviwes></Reviwes>}>Reviwes</Route>
+        <Route path='/dashboard' element={<DashBoard></DashBoard>}>DashBoard</Route>
+        <Route path='/blogs' element={<Blogs></Blogs>}>Blogs</Route>
+        <Route path='/about' element={<About></About>}>About</Route>
+        <Route path='/*' element={<NotFound></NotFound>}></Route>
+      </Routes>
     </div>
   );
 }
