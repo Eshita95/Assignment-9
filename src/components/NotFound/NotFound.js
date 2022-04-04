e2065a4abc592398da463093b685976da4b9b890
eshita95 <eshita.islam95@gmail.com>
2022-04-05 01:23:11 +0600

    not found added

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div>
-            
+        <div className='mt-20'>
+            <h2 className='text-4xl font-bold'>Bad Gateway</h2>
+            <h1 className='text-5xl font-bold mt-8'>404</h1>
         </div>
     );
 };
